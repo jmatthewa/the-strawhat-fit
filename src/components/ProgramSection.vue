38af85d654c4f5b0d85ca6a28664301628378528
jmatthewa <jamesm.atanacio@gmail.com>
2023-04-04 04:07:49 -0700

    add program section

---
+++ src/components/ProgramSection.vue
@@ -0,0 +1,105 @@
+<template>
+   <div class="Programs" id="programs">
+
+        <div class="programs-header">
+            <span class="stroke-text">Explore our</span>
+            <span>Programs</span>
+            <span class="stroke-text">to shape you</span>
+        </div>
+
+
+        <div class="programs-categories"  >
+            <div class="category" v-for="program in programsData" :key="program.heading">
+                <img src="../assets/dumbell.svg" alt="">
+                <span>{{ program.heading }}</span>
+                <span>{{ program.details }}</span>
+    
+                <div class="join-now"><span>Join Now</span><img src="../assets/rightArrow.png" alt="" srcset=""></div>
+            </div>
+         
+    
+        </div>
+        
+
+
+
+   </div>
+</template>
+<script>
+
+import { programsData } from '../data/programsData.js'
+
+export default {
+
+    data() {
+        return {
+            programsData : programsData
+
+        }
+    }
+}
+
+
+</script>
+<style>
+    .Programs {
+        margin-top: 2rem;
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        padding: 0 2rem;
+    }
+
+    .programs-header {
+        display: flex;
+        gap: 5rem;
+        font-weight: bold;
+        font-size: 3rem;
+        justify-content: center;
+        color: white;
+        text-transform: uppercase;
+        font-style: italic;
+    }
+
+    .programs-categories {
+       display: flex;
+        gap: 1rem;
+    }
+    .category {
+        display: flex;
+        flex-direction: column;
+        background-color: gray;
+        padding: 2rem;
+        color: white;
+        justify-content: space-between;
+    }
+    .category>:nth-child(1){
+    
+        width: 2rem;
+        height: 2rem;
+        fill:white;
+    }
+    .category>:nth-child(2){
+        margin-top: 1rem;
+        font-size: 1rem;
+        font-weight: bold;
+    }
+    .category>:nth-child(3){
+        margin-top: 2rem;
+        font-size: 0.9rem;
+        line-height: 25px;
+    }
+    .join-now {
+        margin-top: 1rem;
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+    }
+    .join-now>img {
+        width: 1rem;
+    }
+    .category:hover {
+        background: var(--planCard);
+        cursor: pointer;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <HeroSection />
+    <ProgramSection />
+    <ReasonSection />
   </div>
 </template>
 
 <script>
+
 import HeroSection from '../components/HeroSection.vue'
+import ProgramSection from '../components/ProgramSection.vue'
+import ReasonSection from '../components/ReasonSection.vue'
 
 export default {
   
-  components: { HeroSection
-
+  components: { 
+    HeroSection,
+    ProgramSection,
+    ReasonSection
   }
+  
 }
 </script>
